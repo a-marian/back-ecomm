31d8597c677228f75c40ffab11b994ffaee40a5a
marelas <marela.as@gmail.com>
2025-03-17 22:16:05 -0300

    update CORS configs

--- src/main/java/com/back/ecomm/configuration/SecurityConfig.java
+++ src/main/java/com/back/ecomm/configuration/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.back.ecomm.configuration;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,14 +8,18 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Autowired
@@ -42,9 +47,25 @@ public class SecurityConfig {
     }
 
     @Bean
+    public CorsConfiguration corsConfiguration(){
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowCredentials(true); //allows taking authentication with credentials
+        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:4200"));  // providing the allowed origin details,
+        corsConfig.setAllowedMethods(Collections.singletonList("*")); // allowing all HTTP methods
+        corsConfig.setAllowedHeaders(Collections.singletonList("*")); // allowing all the request headers
+        corsConfig.setMaxAge(Duration.ofMinutes(5L)); // setting the max time till which the allowed origin will not make a preflight request again to check if the CORS is allowed or not
+        return  corsConfig;
+    }
+
+    @Bean
     protected SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws  Exception {
-        httpSecurity.authorizeHttpRequests( auth ->
-                        auth.requestMatchers( "/api/login", "/api/register").permitAll()
+        httpSecurity.cors(corsCustom -> corsCustom.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                         return   corsConfiguration();
+                    }
+                })).authorizeHttpRequests( auth ->
+                        auth.requestMatchers( "/api/login", "/api/register", "/api/products/**").permitAll()
                                 .anyRequest().authenticated())
                 .logout(logout -> logout.invalidateHttpSession(true)
                         .clearAuthentication(true).permitAll());
